6c205afdf322cdc988783f86050cdb0d97d014ae
joseph_ctu <sephxd1234@gmail.com>
2018-03-14 17:45:12 +0100

    stalls solves c-small-2

--- stalls.py
+++ stalls.py
@@ -14,40 +14,54 @@ def solve(case, stalls_people):
     stalls, people = [int(element) for element in stalls_people]
     if stalls == people:
         return "Case #"+str(case + 1)+": " + str(0) + " " + str(0)
-    y = 0
-    z = 0
-    ls_rs = [stalls]
-    for person in range(0, people):
-        response, new_ls_rs = compute(ls_rs)
-        ls_rs = new_ls_rs
-        if person == people - 1:
-            y = max(response)
-            z = min(response)
-    return "Case #"+str(case + 1)+": " + str(y) + " " + str(z)
+    ls_rs, length = compute(stalls)
+    if (people - 1) > length:
+        y, z = nodeCalculator(1)
+    else:
+        y, z = nodeCalculator(ls_rs[people-1])
+
+    return "Case #"+str(case + 1)+": " + str(int(y)) + " " + str(int(z))
 
 
 def get_number_of_cases(row):
     return int(row[0])
 
 
-def compute(ls_rs):
-    max_segment = max(ls_rs)
-    ls_rs.remove(max_segment)
+def nodeCalculator(node):
     a, b = 0, 0
-    if max_segment == 1:
+    if node % 2 == 0:
+        # even
+        a = node/2
+        b = node/2 - 1
+    elif node % 2 != 0:
+        # odd
+        a = int(node/2)
+        b = a
+    return max(a, b), min(a, b)
+
+
+def compute(stalls):
+    flag = True
+    index = 0
+    ls_rs = [stalls]
+    while flag:
+        N = ls_rs[index]
+        if N == 2 or N == 3:
+            # the rest are just ones
+            flag = False
+        if N % 2 == 0:
+            # even
+            a = N/2
+            b = N/2 - 1
+        elif N % 2 != 0:
+            # odd
+            a = int(N/2)
+            b = a
         ls_rs.append(a)
         ls_rs.append(b)
-        return [a, b], ls_rs
-    if max_segment % 2 == 0:
-        # even procedure
-        a = int(max_segment / 2) - 1
-        b = int(max_segment / 2)
-    else:
-        a = int(math.floor((max_segment) / 2))
-        b = a
-    ls_rs.append(a)
-    ls_rs.append(b)
-    return [a, b], ls_rs
+        index += 1
+    sorted_nodes = sorted(ls_rs, key=None, reverse=True)
+    return sorted_nodes, len(sorted_nodes)
 
 
 filename = utils.getFilename()
