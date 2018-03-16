e2c87e081c52c7b9bcc275706cc020f4824c0dca
joseph_ctu <sephxd1234@gmail.com>
2018-03-16 15:59:16 +0100

    Implement optimized tree exploration

--- stalls.py
+++ stalls.py
@@ -14,17 +14,21 @@ def solve(case, stalls_people):
     stalls, people = [int(element) for element in stalls_people]
     if stalls == people:
         return "Case #"+str(case + 1)+": " + str(0) + " " + str(0)
-    ls_rs, length = compute(stalls)
-    if (people - 1) > length:
+    ls_rs, length = compute(stalls, people)
+    if (people - 1) >= length:
         y, z = nodeCalculator(1)
     else:
         y, z = nodeCalculator(ls_rs[people-1])
-
+    print(optimized([5]))
     return "Case #"+str(case + 1)+": " + str(int(y)) + " " + str(int(z))
 
 
 def get_number_of_cases(row):
     return int(row[0])
+
+
+def previous_power_of_2(x):
+    return 1 if x == 0 else int(2**(x).bit_length() / 2)
 
 
 def nodeCalculator(node):
@@ -40,7 +44,7 @@ def nodeCalculator(node):
     return max(a, b), min(a, b)
 
 
-def compute(stalls):
+def compute(stalls, people):
     flag = True
     index = 0
     ls_rs = [stalls]
@@ -60,8 +64,24 @@ def compute(stalls):
         ls_rs.append(a)
         ls_rs.append(b)
         index += 1
+        if index > people:
+            flag = False
     sorted_nodes = sorted(ls_rs, key=None, reverse=True)
     return sorted_nodes, len(sorted_nodes)
+
+
+def optimized(nodes):
+    new_nodes = [breaker(node) for node in nodes]
+    return [item for sublist in new_nodes for item in sublist]
+
+
+#flat_list = [item for sublist in l for item in sublist]
+
+def breaker(node):
+    if node % 2 == 0:
+        return [int(node/2 - 1), int(node/2)]
+    else:
+        return [int(node/2), int(node/2)]
 
 
 filename = utils.getFilename()
